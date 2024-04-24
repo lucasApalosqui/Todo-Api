51d5e379619f7a6afda0ad10837428ea57fb8bdb
LucasApalosqui <lucas@apalosqui@gmail.com>
2024-04-24 15:48:11 -0300

    :sparkles: feat: Initial Handler Test

---
+++ Todo.Domain.Tests/HandlerTests/CreateTodoHandlerTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Todo.Domain.Tests.HandlerTests
+{
+    [TestClass]
+    public class CreateTodoHandlerTests
+    {
+        [TestMethod]
+        public void Dado_um_comando_invalido_deve_interromper_a_execucao()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void Dado_um_comando_valido_deve_criar_a_tarefa()
+        {
+            Assert.Fail();
+        }
+    }
+}
