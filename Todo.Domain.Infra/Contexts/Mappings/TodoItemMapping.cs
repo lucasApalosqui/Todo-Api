905c5d933b9c399bb288eadd5bb49608b0bebee0
LucasApalosqui <lucas@apalosqui@gmail.com>
2024-04-24 21:24:11 -0300

    :sparkles: feat: Install entityframeworkCore.Relational and mapping todoItem

---
+++ Todo.Domain.Infra/Contexts/Mappings/TodoItemMapping.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Todo.Domain.Entities;
+
+namespace Todo.Domain.Infra.Contexts.Mappings
+{
+    public class TodoItemMapping : IEntityTypeConfiguration<TodoItem>
+    {
+        public void Configure(EntityTypeBuilder<TodoItem> builder)
+        {
+            builder.ToTable("Todo");
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.User).HasMaxLength(128).HasColumnType("varchar(120)");
+
+            builder.Property(x => x.Title).HasMaxLength(160).HasColumnType("varchar(160)");
+
+            builder.Property(x => x.Done).HasColumnType("bit");
+
+            builder.Property(x => x.Date).HasColumnType("DATETIME");
+
+            builder.HasIndex(x => x.User);
+
+            
+        }
+    }
+}
--- Todo.Domain.Infra/Contexts/TodoContext.cs
+++ Todo.Domain.Infra/Contexts/TodoContext.cs
@@ -1,13 +1,21 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Todo.Domain.Entities;
+using Todo.Domain.Infra.Contexts.Mappings;
 
 namespace Todo.Domain.Infra.Contexts
 {
     public class TodoContext : DbContext
     {
-        public TodoContext(DbContextOptions<TodoContext> options) : base(options) { }
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
 
         public DbSet<TodoItem> Todos { get; set; }
-       
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new TodoItemMapping());
+        }
     }
 }
